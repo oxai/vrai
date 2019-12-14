8068ae3bdc59da07ff6ab6ec76717f16980bf437
Guillermo Valle <guillefix@gmail.com>
2019-12-14 19:56:45 +0000

    'improvements to ddpg'

--- agent/curiosity/learning_progress_ddpg.py
+++ agent/curiosity/learning_progress_ddpg.py
@@ -21,6 +21,7 @@ def MLP(input_dim, output_dim, number_layers=2, hidden_dim=None):
     layers = sum(
         [[nn.Linear(input_dim, hidden_dim), nn.ReLU()]]
         + [[nn.Linear(hidden_dim, hidden_dim), nn.ReLU(), nn.BatchNorm1d(hidden_dim)] for i in range(number_layers-2)]
+        #+ [[nn.Linear(hidden_dim, hidden_dim), nn.ReLU()] for i in range(number_layers-2)]
         + [[nn.Linear(hidden_dim, output_dim)]]
         , [])
 
@@ -37,37 +38,55 @@ class GOALRNN(nn.Module):
         self.action_decoder = MLP(goal_dim+observation_dim, action_dim, hidden_dim=1024)
         self.q_value_decoder = MLP(goal_dim+observation_dim+action_dim, 1, number_layers=1, hidden_dim=1024)
         self.qlp_decoder = MLP(observation_dim+goal_dim, 1)
+        self.pen_vars_slice = slice(54,61)
 
     def forward(self, observations):
-        pen_vars_slice = slice(54,61)
         pen_pos_center = torch.Tensor([1.0,0.90,0.15]).unsqueeze(0).unsqueeze(0)
+
+        noisy_goals = self.compute_noisy_goals(observations)
+        #print(observations,noisy_goals)
+        actions = self.compute_actions(noisy_goals, observations)
+        #print(actions)
+        if np.random.rand() < 0.1:
+            noisy_actions = actions + 5*torch.randn_like(actions)
+        else:
+            noisy_actions = actions + 0.1*torch.randn_like(actions)
+        #noisy_actions = actions
+        #values = self.compute_q_value(noisy_goals, observations, noisy_actions)
+        #lp_values = self.compute_qlp(observations, noisy_goals)
+        return actions, noisy_actions, noisy_goals#, values, lp_values
+
+    
+    def compute_goals(self,observations):
+        pen_vars_slice = self.pen_vars_slice
         goals = self.goal_decoder(observations)
-        goals = 100*torch.tanh(goals*0.001)
         pen_pos = observations[:,:,pen_vars_slice][...,:3]
         pen_rot = observations[:,:,pen_vars_slice][...,3:]
         rot_goal = goals[:,:,3:]
         rel_rot_goal = (rot_goal-pen_rot)*0.1+pen_rot
         goals = torch.cat([(goals[:,:,:3]-pen_pos)*0.002+pen_pos,(rel_rot_goal)/torch.norm(rel_rot_goal)], dim=2)
-
-        noisy_goals = goals + 0.01*torch.randn_like(goals)
-
-        actions = self.compute_actions(noisy_goals, observations)
-        noisy_actions = actions + 0.01*torch.randn_like(actions)
-        values = self.compute_q_value(noisy_goals, observations, noisy_actions)
-        lp_values = self.compute_qlp(observations, noisy_goals)
-        return actions, noisy_actions, goals, noisy_goals, values, lp_values
+        return goals
+    
+    def compute_noisy_goals(self,observations):
+        goals = self.compute_goals(observations)
+        if np.random.rand() < 0.2:
+            noisy_goals = goals + 0.05*torch.randn_like(goals)
+        else:
+            noisy_goals = goals + 0.01*torch.randn_like(goals)
+        return noisy_goals
 
     def compute_q_value(self, goals, observations, actions):
         values = self.q_value_decoder(torch.cat([goals,observations,actions], dim=2))
         values = values - 1 # learn the difference between the value and -1, because at the beginning most values will be close to -1
-        values = torch.tanh(values)
+        #values = torch.tanh(values)
         return values
 
     def compute_qlp(self, observations, goals):
         values = self.qlp_decoder(torch.cat([observations,goals], dim=2))
-        values = torch.tanh(values)
+        #values = torch.tanh(values)
         return values
 
     def compute_actions(self, goals, observations):
         actions = self.action_decoder(torch.cat([goals, observations], dim=2))
+        #print(actions)
         return actions
