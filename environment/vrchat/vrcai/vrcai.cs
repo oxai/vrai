1b201e92bdf6efd91727f3db2947d1ccf677b6a4
Guillermo Valle <guillefix@gmail.com>
2019-12-30 02:20:25 +0100

    vrcmod gets player position and hands position; should next try to get head position and orientations

--- environment/vrchat/vrcai/vrcai.cs
+++ environment/vrchat/vrcai/vrcai.cs
@@ -35,7 +35,7 @@ namespace ExampleModule
 		}
 
 		
-		public void Update()
+		public void FixedUpdate()
 		{
             this.GetPlayerPositions();
 		}
@@ -43,12 +43,17 @@ namespace ExampleModule
 		{
 			foreach (Player player in PlayerManager.GetAllPlayers())
 			{
-                Vector3 positions = player.vrcPlayer.avatarGameObject.transform.position;
-                using (System.IO.StreamWriter file =
-                new System.IO.StreamWriter(@"player_positions.txt", true))
-                    {
-                        file.WriteLine(positions.ToString());
-                    }
+                String name = player.vrcPlayer.namePlate.mainText.text; //not sure if this is the best way to get username
+                Vector3 position = player.vrcPlayer.avatarGameObject.transform.position;
+                Transform chest = player.vrcPlayer.avatarGameObject.transform.Find("Armature").Find("Hips").Find("Spine").Find("Chest");
+                Transform right_hand = chest.Find("Right_shoulder").Find("Right_arm").Find("Right_elbow").Find("Right_wrist");
+                Vector3 right_hand_position = right_hand.position;
+                Transform left_hand = chest.Find("Left_shoulder").Find("Left_arm").Find("Left_elbow").Find("Left_wrist");
+                Vector3 left_hand_position = left_hand.position;
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter("player_"+name+"_data.txt", true))
+                {
+                    file.WriteLine(position.ToString("F6") + "\t" + left_hand_position.ToString("F6") + "\t" + right_hand_position.ToString("F6"));
+                }
             }
 		}
 
