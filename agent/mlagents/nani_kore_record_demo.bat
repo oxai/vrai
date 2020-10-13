1bbcbed1aa6dbb3b6c9d1c6f02cbd036a5e0f4e4
Guillermo Valle <guillefix@gmail.com>
2020-10-14 00:06:48 +0100

    built env

--- agent/mlagents/nani_kore_record_demo.bat
+++ agent/mlagents/nani_kore_record_demo.bat
@@ -1,5 +1,5 @@
 Taskkill /IM Neos.exe /F
 start "" /d "C:\Program Files (x86)\Steam\steamapps\common\NeosVR\" run_neos_vrai_desktop_record_demo.bat
 timeout /t 50
-mlagents-learn eco_npc_config.yaml --run-id=test_eco_demo --env="..\..\environment\neos\built_env\Unity Environment"
+mlagents-learn eco_npc_config.yaml --run-id=test_eco_demo --env="..\..\environment\neos\built_env\Unity Environment" --force
 Taskkill /IM Neos.exe /F
