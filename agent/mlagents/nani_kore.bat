58f9264f385869db4e115cb43567d769f54cf89f
Guillermo Valle <guillefix@gmail.com>
2020-10-18 18:21:39 +0100

    tweak to script

--- agent/mlagents/nani_kore.bat
+++ agent/mlagents/nani_kore.bat
@@ -2,11 +2,11 @@ tasklist /FI "IMAGENAME eq Neos.exe" 2>NUL | find /I /N "Neos.exe">NUL
 if NOT "%ERRORLEVEL%"=="0" start "" /d "C:\Program Files (x86)\Steam\steamapps\common\NeosVR\" run_neos_vrai_desktop.bat 
 tasklist /FI "IMAGENAME eq Neos.exe" 2>NUL | find /I /N "Neos.exe">NUL
 if NOT "%ERRORLEVEL%"=="0" timeout /t 50
-mlagents-learn eco_npc_config.yaml --run-id=test_eco_demo_new2 --env="..\..\environment\neos\built_env\Unity Environment" --resume --time-scale=2.0 && Taskkill /IM Neos.exe /F
+mlagents-learn eco_npc_config.yaml --run-id=test_eco_demo_new3 --env="..\..\environment\neos\built_env\Unity Environment" --time-scale=2.0 && Taskkill /IM Neos.exe /F
 
 :loop
 Taskkill /IM Neos.exe /F
 start "" /d "C:\Program Files (x86)\Steam\steamapps\common\NeosVR\" run_neos_vrai_desktop.bat 
 timeout /t 50
-mlagents-learn eco_npc_config.yaml --run-id=test_eco_demo_new2 --env="..\..\environment\neos\built_env\Unity Environment" --resume --time-scale=2.0 && Taskkill /IM Neos.exe /F
+mlagents-learn eco_npc_config.yaml --run-id=test_eco_demo_new3 --env="..\..\environment\neos\built_env\Unity Environment" --resume --time-scale=2.0 && Taskkill /IM Neos.exe /F
 goto loop
